ef7f8631c21f3e6bc419bfedf00fd1f6681e2a61
aqild65 <159115802+aqild65@users.noreply.github.com>
2024-07-02 23:09:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["⚜️𝐃ᴋᴀᴛᴀɴᴀ Community","🌐?discord.gg/dkatana","ⓒPowered by 2ez4QiLL⚡"];
+const statusMessages = ["⚜️𝐃ᴋᴀᴛᴀɴᴀ Community","ⓒPowered by 2ez4QiLL⚡"];
 
 
 let currentIndex = 0;
